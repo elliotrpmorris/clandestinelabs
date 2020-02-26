0560339c5ed0c3934b5a8ae00935eb94adacab91
Elliot <56415058+elliotrpmorris@users.noreply.github.com>
2020-02-26 00:27:07 +0000

    remove broken global style

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Nav from './Common/Nav';
 import Footer from './Common/Footer';
-import GlobalStyle from './Common/GlobalStyles';
+import './App.css';
 
 function App() {
   return (
     <main>
-      <GlobalStyle/>
       <Nav/>
       <Footer/>
     </main>
--- src/components/Common/GlobalStyles.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from 'styled-components'
-
-const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Sniglet|Work+Sans');
-  body {
-    font-family: 'Work Sans', sans-serif;
-  }
-`
-export default GlobalStyle;
