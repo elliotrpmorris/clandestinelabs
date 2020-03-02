1267d4b8ac58be8b373bd34309dd31fcadeeb617
Elliot <56415058+elliotrpmorris@users.noreply.github.com>
2020-03-02 18:14:07 +0000

    better nav close

--- src/components/Common/Nav.js
+++ src/components/Common/Nav.js
@@ -55,24 +55,57 @@ export const Page = styled(Link)`
   }
 `;
 
-function Nav() {
-  return (
-    <Router>
-      <div id="outer-container">
-        <Menu isOpen={false} pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" } disableAutoFocus right>
-          <Page to="/">Home</Page> 
-          <Page to="/about">About</Page> 
-          <Page to="/services">Services</Page>      
-          <Page to="/Work">Projects</Page>        
-          <Page to="/contact">Contact</Page>      
-        </Menu>
-      </div>
-      <Route path="/" exact component={Home} />
-      <Route path="/about"  component={About} />
-      <Route path="/services"  component={Services} />
-      <Route path="/contact"  component={Contact} />
-      <Route path="/work"  component={Work} />
-    </Router>
-  );
+class Nav extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      menuOpen: false
+    }
+  }
+
+  // This keeps your state in sync with the opening/closing of the menu
+  // via the default means, e.g. clicking the X, pressing the ESC key etc.
+  handleStateChange (state) {
+    this.setState({menuOpen: state.isOpen})  
+  }
+  
+  // This can be used to close the menu, e.g. when a user clicks a menu item
+  closeMenu () {
+    this.setState({menuOpen: false})
+  }
+
+  // This can be used to toggle the menu, e.g. when using a custom icon
+  // Tip: You probably want to hide either/both default icons if using a custom icon
+  // See https://github.com/negomi/react-burger-menu#custom-icons
+  toggleMenu () {
+    this.setState(state => ({menuOpen: !state.menuOpen}))
+  }
+  render () {
+    return (
+      <Router>
+        <div id="outer-container">
+          <Menu 
+            isOpen={this.state.menuOpen}
+            onStateChange={(state) => this.handleStateChange(state)}
+            pageWrapId={ "page-wrap" } 
+            outerContainerId={ "outer-container" } 
+            disableAutoFocus 
+            right
+          >
+            <Page onClick={() => this.closeMenu()} to="/">Home</Page> 
+            <Page onClick={() => this.closeMenu()} to="/about">About</Page> 
+            <Page onClick={() => this.closeMenu()} to="/services">Services</Page>      
+            <Page onClick={() => this.closeMenu()} to="/Work">Projects</Page>        
+            <Page onClick={() => this.closeMenu()} to="/contact">Contact</Page>      
+          </Menu>
+        </div>
+        <Route path="/" exact component={Home} />
+        <Route path="/about"  component={About} />
+        <Route path="/services"  component={Services} />
+        <Route path="/contact"  component={Contact} />
+        <Route path="/work"  component={Work} />
+      </Router>
+    );
+  }
 }
 export default Nav;
