1b03387c3e9a1c8bb82446c28af71cf0d72f6bc6
Elliot <56415058+elliotrpmorris@users.noreply.github.com>
2020-05-12 11:37:06 +0100

    add new work item

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import styled from 'styled-components';
-import {Grid, Col, Row} from 'react-styled-flexboxgrid';
-import Project from './Project';
+import React from "react";
+import styled from "styled-components";
+import { Grid, Col, Row } from "react-styled-flexboxgrid";
+import Project from "./Project";
 
-export const Title = styled.h1`      
-    color: #20232a;  
-    display: flex;
-    justify-content: center;
-    font-size: 3.5rem;
-    margin-bottom: 0;
-    @media (min-width: 414px) {
-      font-size: 4rem;
-    }
+export const Title = styled.h1`
+  color: #20232a;
+  display: flex;
+  justify-content: center;
+  font-size: 3.5rem;
+  margin-bottom: 0;
+  @media (min-width: 414px) {
+    font-size: 4rem;
+  }
 `;
 
-export const SubTitle = styled.p`      
-    color: #20232a;  
-    display: flex;
-    justify-content: center;
-    font-size: 1rem;
-    @media (min-width: 415px) {
-      font-size: 1.1rem;
-    }
+export const SubTitle = styled.p`
+  color: #20232a;
+  display: flex;
+  justify-content: center;
+  font-size: 1rem;
+  @media (min-width: 415px) {
+    font-size: 1.1rem;
+  }
 `;
 
 function About() {
   return (
     <section id="page-wrap">
-       <Grid>
+      <Grid>
         <Row>
           <Col xs={12}>
             <Title>Projects</Title>
@@ -39,26 +39,34 @@ function About() {
         <Row>
           <Col xs={12} md={6}>
             <Project
-                title="Leeds Rhinos"
-                description="On this Elliot worked on the front end templates, which consisted of the styling and layout. He was tasked with
+              title="Leeds Rhinos"
+              description="On this Elliot worked on the front end templates, which consisted of the styling and layout. He was tasked with
                 creating the site based on provided design templates."
-                link="https://therhinos.co.uk"
-              />
+              link="https://therhinos.co.uk"
+            />
           </Col>
           <Col xs={12} md={6}>
             <Project
-                title="Asda Money"
-                description="Elliot created the current home page built from a given design template. which is fully mobile responsive."
-                link="https://money.asda.com"
-              />
+              title="Asda Money"
+              description="Elliot created the current home page built from a given design template. which is fully mobile responsive."
+              link="https://money.asda.com"
+            />
           </Col>
           <Col xs={12} md={6}>
             <Project
-                title="Lesson's for less"
-                description="This site allows users to compare driving lesson prices. Elliot was involved in designing of the website and
+              title="Lesson's for less"
+              description="This site allows users to compare driving lesson prices. Elliot was involved in designing of the website and
                 the creation of templates."
-                link="https://lessons4less.co.uk"
-              />
+              link="https://lessons4less.co.uk"
+            />
+          </Col>
+          <Col xs={12} md={6}>
+            <Project
+              title="Treat Wisely"
+              description="This was a static site built using Angular. This site is used to showcase their product and offer give the business an online presence. 
+              The site shows reviews from Facebook and a gallery, in the future, this site can be expanded to take payments when the business grows."
+              link="https://treatwisely.co.uk"
+            />
           </Col>
         </Row>
       </Grid>
